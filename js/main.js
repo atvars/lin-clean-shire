10551f0f8bbac35c9b30d749699edd76844cf5dd
atvars <avelavs@gmail.com>
2025-03-12 02:50:05 +0000

    added html for contact section

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ window.onclick = function(event) {
 
 // service 2
 // Get the modal
-var modal = document.getElementById("service2");
+var modal2 = document.getElementById("service2");
 
 // Get the button that opens the modal
 var btn2 = document.getElementById("openModalBtnService2");
@@ -56,18 +56,18 @@ var span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal
 btn2.onclick = function() {
-    modal.style.display = "block";
+    modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
-    modal.style.display = "none";
+    modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
 
@@ -83,17 +83,17 @@ var span3 = document.getElementsByClassName("close")[2];
 
 // When the user clicks the button, open the modal
 btn3.onclick = function() {
-    modal.style.display = "block";
+    modal3.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span3.onclick = function() {
-    modal.style.display = "none";
+    modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal3) {
+        modal3.style.display = "none";
     }
 }
